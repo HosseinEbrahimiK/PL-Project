3e12819b1076c03b679572505734161afeb85841
ali-bana <alibanaei@live.com>
2020-07-31 20:31:31 +0430

    func parser bitches
    parser with func

--- Interpreter.rkt
+++ Interpreter.rkt
@@ -1,37 +1,33 @@
 #lang racket
 (require (lib "eopl.ss" "eopl"))
+(require racket/include)
+(include "parser.rkt")
+(include "env_code.rkt")
+(include "value-of.rkt")
 
-(define (comp List) (cond
-                   [(null? List) '()]
-                   [(number? (car List)) (append (list(- (car List))) (comp (cdr List)))]
-                   [(boolean? (car List)) (append (list(not (car List))) (comp (cdr List)))]
-                   [(list? (car List)) (append (list(comp (car List))) (comp (cdr List)))]
-                   [else (begin (display (car List)) (display " is the element of array: can not be negative."))]
-                  ))
 
-(define value-of-cexp
-  (lambda (expression env)
-    (cases cexp expression
-      
-      (cexp-comp (expr)
-                 (let ([val (value-of-cexp expr env)])
-                    (cond
-                     [(number? val) (- val)]
-                     [(boolean? val) (not val)]
-                     [(list? val) (comp val)]
-                     [else (display "This kind of data can not be negative.")])
-                    ))
 
-      (cexp-par (expr) (value-of-exp expr))
-                
-      (cexp-num (num-exp) num-exp)
 
-      (cexp-null (null-exp) null-exp)
+(define (interperet in)
+  (define lex-this (lambda (lexer input) (lambda () (lexer input))))
+  (define my-lexer (lex-this simple-math-lexer (open-input-string (file->string in #:mode 'text))))
+  (with-handlers (
+                  [(lambda (v)  (list? v)) (lambda (v) (list-ref v 1))])
+    (value-of-command (let ((parser-res (simple-math-parser my-lexer))) parser-res) (empty-env))
+  )
+)
 
-      (cexp-var (var) var)
 
-      (cexp-bool (bool-exp) bool-exp)
 
-      (cexp-string (str) str))))
+
+
+
+
+
+(interperet "prog.txt")
+
+
+
+
       
                    
